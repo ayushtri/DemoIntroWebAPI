4995ab5e5b6c6e64ad17497176ed1e813691adc3
Ayush Tripathi <tripathi.at02@gmail.com>
2024-11-19 15:54:52 +0530

    GetEmps

--- DemoIntroWebAPIApp/Controllers/EmployeeController.cs
+++ DemoIntroWebAPIApp/Controllers/EmployeeController.cs
@@ -8,6 +8,55 @@ namespace DemoIntroWebAPIApp.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private static List<Employee> _employee = new List<Employee>
+        {
+            new Employee() { Id = 123, Name = "Ayush", Gender = "male", Department = "IT", Age = 22, City = "Bhopal" },
+            new Employee() { Id = 124, Name = "Nandini", Gender = "female", Department = "CSE", Age = 22, City = "Arizona" },
+            new Employee() { Id = 125, Name = "Satvik", Gender = "male", Department = "CSE", Age = 22, City = "Bhopal" },
+            new Employee() { Id = 126, Name = "Mansi", Gender = "female", Department = "Idk", Age = 22, City = "Delhi" },
+            new Employee() { Id = 127, Name = "Somesh", Gender = "male", Department = "Idk", Age = 22, City = "Bhopal" },
+            new Employee() { Id = 128, Name = "Satvik", Gender = "male", Department = "CSE", Age = 22, City = "Bhopal" },
+            new Employee() { Id = 129, Name = "Sarabpreet", Gender = "male", Department = "CSE", Age = 22, City = "Bhopal" },
+            new Employee() { Id = 130, Name = "Tanya", Gender = "female", Department = "Idk", Age = 22, City = "Delhi" }
+        };
+
+        [HttpGet]
+        public IActionResult GetEmployee([FromQuery(Name = "Dept")]string Department)
+        {
+            var employee = _employee.Where(emp => emp.Department.Equals(Department, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (employee.Count > 0) 
+            { 
+                return Ok(employee);
+            }
+            return NotFound($"No Employee found for Department: {Department}");
+        }
+
+        [Route("Emp/Filter")]
+        [HttpGet]
+        public IActionResult GetEmps([FromQuery] EmpSearch empSearch)
+        {
+            var empFilters = new List<Employee>();
+
+            if (empSearch != null)
+            {
+                empFilters = _employee.Where(emp =>
+                    emp.Department.Equals(empSearch.Department, StringComparison.OrdinalIgnoreCase) ||
+                    emp.Gender.Equals(empSearch.Gender, StringComparison.OrdinalIgnoreCase)
+                ).ToList();
+
+                if (empFilters.Count > 0)
+                {
+                    return Ok(empFilters);
+                }
+                else
+                {
+                    return NotFound($"No Employee found for Department {empSearch?.Department} or Gender {empSearch?.Gender}");
+                }
+            }
+
+            return BadRequest("Invalid Filter");
+        }
+
         [Route("Name")]
         [HttpGet]
         public string GetName()
---
+++ DemoIntroWebAPIApp/Models/EmpSearch.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoIntroWebAPIApp.Models
+{
+    public class EmpSearch
+    {
+        public string Gender { get; set; }
+        public string Department { get; set; }
+    }
+}
